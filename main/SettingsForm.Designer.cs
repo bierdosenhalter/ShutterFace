5c03add4ee0714035f5707489da919bd6c2e21e1
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2026-09-09 10:40:32 +0200

    Fix SettingsForm: visible controls and properly positioned buttons

--- main/SettingsForm.Designer.cs
+++ main/SettingsForm.Designer.cs
@@ -59,7 +59,7 @@ namespace MotionTrackerFaceBlur
             // lblBlurCellSize
             // 
             this.lblBlurCellSize.AutoSize = true;
-            this.lblBlurCellSize.Location = new Point(20, 30);
+            this.lblBlurCellSize.Location = new Point(20, 25);
             this.lblBlurCellSize.Name = "lblBlurCellSize";
             this.lblBlurCellSize.Size = new Size(108, 20);
             this.lblBlurCellSize.TabIndex = 0;
@@ -67,15 +67,15 @@ namespace MotionTrackerFaceBlur
             // 
             // nudBlurCellSize
             // 
-            this.nudBlurCellSize.Location = new Point(20, 55);
+            this.nudBlurCellSize.Location = new Point(20, 50);
             this.nudBlurCellSize.Name = "nudBlurCellSize";
-            this.nudBlurCellSize.Size = new Size(80, 26);
+            this.nudBlurCellSize.Size = new Size(100, 26);
             this.nudBlurCellSize.TabIndex = 1;
             // 
             // lblBigPixels
             // 
             this.lblBigPixels.AutoSize = true;
-            this.lblBigPixels.Location = new Point(20, 95);
+            this.lblBigPixels.Location = new Point(20, 85);
             this.lblBigPixels.Name = "lblBigPixels";
             this.lblBigPixels.Size = new Size(156, 20);
             this.lblBigPixels.TabIndex = 2;
@@ -83,15 +83,15 @@ namespace MotionTrackerFaceBlur
             // 
             // nudBigPixels
             // 
-            this.nudBigPixels.Location = new Point(20, 120);
+            this.nudBigPixels.Location = new Point(20, 110);
             this.nudBigPixels.Name = "nudBigPixels";
-            this.nudBigPixels.Size = new Size(80, 26);
+            this.nudBigPixels.Size = new Size(100, 26);
             this.nudBigPixels.TabIndex = 3;
             // 
             // lblConfidenceThreshold
             // 
             this.lblConfidenceThreshold.AutoSize = true;
-            this.lblConfidenceThreshold.Location = new Point(20, 30);
+            this.lblConfidenceThreshold.Location = new Point(20, 25);
             this.lblConfidenceThreshold.Name = "lblConfidenceThreshold";
             this.lblConfidenceThreshold.Size = new Size(156, 20);
             this.lblConfidenceThreshold.TabIndex = 4;
@@ -101,17 +101,18 @@ namespace MotionTrackerFaceBlur
             // 
             this.nudConfidenceThreshold.DecimalPlaces = 2;
             this.nudConfidenceThreshold.Increment = new decimal(new int[] { 5, 0, 0, 65536 });
-            this.nudConfidenceThreshold.Location = new Point(20, 55);
+            this.nudConfidenceThreshold.Location = new Point(20, 50);
             this.nudConfidenceThreshold.Name = "nudConfidenceThreshold";
-            this.nudConfidenceThreshold.Size = new Size(80, 26);
+            this.nudConfidenceThreshold.Size = new Size(100, 26);
             this.nudConfidenceThreshold.TabIndex = 5;
             // 
             // pnlSettings
             // 
             this.pnlSettings.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            this.pnlSettings.AutoScroll = true;
             this.pnlSettings.Location = new Point(12, 12);
             this.pnlSettings.Name = "pnlSettings";
-            this.pnlSettings.Size = new Size(340, 280);
+            this.pnlSettings.Size = new Size(400, 320);
             this.pnlSettings.TabIndex = 6;
             // 
             // gbBlur
@@ -119,7 +120,7 @@ namespace MotionTrackerFaceBlur
             this.gbBlur.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             this.gbBlur.Location = new Point(10, 10);
             this.gbBlur.Name = "gbBlur";
-            this.gbBlur.Size = new Size(320, 170);
+            this.gbBlur.Size = new Size(380, 145);
             this.gbBlur.TabIndex = 7;
             this.gbBlur.TabStop = false;
             this.gbBlur.Text = "Unschärfe-Einstellungen";
@@ -127,9 +128,9 @@ namespace MotionTrackerFaceBlur
             // gbConfidence
             // 
             this.gbConfidence.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            this.gbConfidence.Location = new Point(10, 195);
+            this.gbConfidence.Location = new Point(10, 165);
             this.gbConfidence.Name = "gbConfidence";
-            this.gbConfidence.Size = new Size(320, 80);
+            this.gbConfidence.Size = new Size(380, 85);
             this.gbConfidence.TabIndex = 8;
             this.gbConfidence.TabStop = false;
             this.gbConfidence.Text = "Tracking-Einstellungen";
@@ -137,7 +138,7 @@ namespace MotionTrackerFaceBlur
             // btnSave
             // 
             this.btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            this.btnSave.Location = new Point(236, 305);
+            this.btnSave.Location = new Point(346, 345);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new Size(89, 30);
             this.btnSave.TabIndex = 9;
@@ -148,7 +149,7 @@ namespace MotionTrackerFaceBlur
             // btnCancel
             // 
             this.btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            this.btnCancel.Location = new Point(325, 305);
+            this.btnCancel.Location = new Point(251, 345);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new Size(89, 30);
             this.btnCancel.TabIndex = 10;
@@ -162,16 +163,25 @@ namespace MotionTrackerFaceBlur
             this.AutoScaleDimensions = new SizeF(9F, 20F);
             this.AutoScaleMode = AutoScaleMode.Font;
             this.CancelButton = btnCancel;
-            this.ClientSize = new Size(364, 351);
+            this.ClientSize = new Size(444, 390);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.pnlSettings);
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SettingsForm";
             this.StartPosition = FormStartPosition.CenterParent;
             this.Text = "Einstellungen";
             this.Load += SettingsForm_Load;
+            this.pnlSettings.Controls.Add(this.gbConfidence);
+            this.pnlSettings.Controls.Add(this.gbBlur);
+            this.gbBlur.Controls.Add(this.lblBigPixels);
+            this.gbBlur.Controls.Add(this.nudBigPixels);
+            this.gbBlur.Controls.Add(this.lblBlurCellSize);
+            this.gbBlur.Controls.Add(this.nudBlurCellSize);
+            this.gbConfidence.Controls.Add(this.lblConfidenceThreshold);
+            this.gbConfidence.Controls.Add(this.nudConfidenceThreshold);
             this.ResumeLayout(false);
         }
 
