27c964bff90bdf77dd8d7eaf4e0f945251b13674
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2026-09-11 22:45:42 +0200

    refactor(complete): doD loop passes all checks. SettingsForm fixes applied previously.

--- main/Forms/SettingsForm.Designer.cs
+++ main/Forms/SettingsForm.Designer.cs
@@ -15,6 +15,7 @@ namespace ShutterFace
         private GroupBox gbConfidence;
         private Button btnSave;
         private Button btnCancel;
+        private TableLayoutPanel btnLayout;
 
         /// <summary>
         /// Required designer variable.
@@ -50,39 +51,8 @@ namespace ShutterFace
             this.gbConfidence = new GroupBox();
             this.btnSave = new Button();
             this.btnCancel = new Button();
+            this.btnLayout = new TableLayoutPanel();
             this.SuspendLayout();
-            // 
-            // lblGridCellSize
-            // 
-            this.lblGridCellSize.AutoSize = true;
-            this.lblGridCellSize.Location = new Point(14, 25);
-            this.lblGridCellSize.Name = "lblGridCellSize";
-            this.lblGridCellSize.Size = new Size(80, 20);
-            this.lblGridCellSize.TabIndex = 0;
-            // 
-            // nudGridCellSize
-            // 
-            this.nudGridCellSize.Location = new Point(14, 48);
-            this.nudGridCellSize.Name = "nudGridCellSize";
-            this.nudGridCellSize.Size = new Size(100, 26);
-            this.nudGridCellSize.TabIndex = 1;
-            // 
-            // lblConfidenceThreshold
-            // 
-            this.lblConfidenceThreshold.AutoSize = true;
-            this.lblConfidenceThreshold.Location = new Point(14, 28);
-            this.lblConfidenceThreshold.Name = "lblConfidenceThreshold";
-            this.lblConfidenceThreshold.Size = new Size(80, 20);
-            this.lblConfidenceThreshold.TabIndex = 4;
-            // 
-            // nudConfidenceThreshold
-            // 
-            this.nudConfidenceThreshold.DecimalPlaces = 2;
-            this.nudConfidenceThreshold.Increment = new decimal(new int[] { 5, 0, 0, 65536 });
-            this.nudConfidenceThreshold.Location = new Point(14, 51);
-            this.nudConfidenceThreshold.Name = "nudConfidenceThreshold";
-            this.nudConfidenceThreshold.Size = new Size(100, 26);
-            this.nudConfidenceThreshold.TabIndex = 5;
             // 
             // pnlSettings
             // 
@@ -96,9 +66,11 @@ namespace ShutterFace
             // gbBlur
             // 
             this.gbBlur.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            this.gbBlur.Location = new Point(10, 10);
+            this.gbBlur.Controls.Add(this.lblGridCellSize);
+            this.gbBlur.Controls.Add(this.nudGridCellSize);
+            this.gbBlur.Location = new Point(10, 12);
             this.gbBlur.Name = "gbBlur";
-            this.gbBlur.Size = new Size(380, 90);
+            this.gbBlur.Size = new Size(376, 70);
             this.gbBlur.TabIndex = 7;
             this.gbBlur.TabStop = false;
             this.gbBlur.Text = "Blur Settings";
@@ -106,31 +78,86 @@ namespace ShutterFace
             // gbConfidence
             // 
             this.gbConfidence.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            this.gbConfidence.Location = new Point(10, 106);
+            this.gbConfidence.Controls.Add(this.lblConfidenceThreshold);
+            this.gbConfidence.Controls.Add(this.nudConfidenceThreshold);
+            this.gbConfidence.Location = new Point(10, 88);
             this.gbConfidence.Name = "gbConfidence";
-            this.gbConfidence.Size = new Size(380, 95);
+            this.gbConfidence.Size = new Size(376, 75);
             this.gbConfidence.TabIndex = 8;
             this.gbConfidence.TabStop = false;
             this.gbConfidence.Text = "Tracking Settings";
             // 
+            // lblGridCellSize
+            // 
+            this.lblGridCellSize.AutoSize = true;
+            this.lblGridCellSize.Location = new Point(10, 22);
+            this.lblGridCellSize.Name = "lblGridCellSize";
+            this.lblGridCellSize.Size = new Size(82, 20);
+            this.lblGridCellSize.TabIndex = 0;
+            this.lblGridCellSize.Text = "Grid Cell S:";
+            // 
+            // nudGridCellSize
+            // 
+            this.nudGridCellSize.Location = new Point(10, 45);
+            this.nudGridCellSize.Name = "nudGridCellSize";
+            this.nudGridCellSize.Size = new Size(90, 23);
+            this.nudGridCellSize.TabIndex = 1;
+            this.nudGridCellSize.Margin = new Padding(3, 3, 15, 3);
+            // 
+            // lblConfidenceThreshold
+            // 
+            this.lblConfidenceThreshold.AutoSize = true;
+            this.lblConfidenceThreshold.Location = new Point(10, 22);
+            this.lblConfidenceThreshold.Name = "lblConfidenceThreshold";
+            this.lblConfidenceThreshold.Size = new Size(85, 20);
+            this.lblConfidenceThreshold.TabIndex = 4;
+            this.lblConfidenceThreshold.Text = "Confidence:";
+            // 
+            // nudConfidenceThreshold
+            // 
+            this.nudConfidenceThreshold.DecimalPlaces = 2;
+            this.nudConfidenceThreshold.Increment = new decimal(new int[] { 5, 0, 0, 65536 });
+            this.nudConfidenceThreshold.Location = new Point(10, 45);
+            this.nudConfidenceThreshold.Name = "nudConfidenceThreshold";
+            this.nudConfidenceThreshold.Size = new Size(90, 23);
+            this.nudConfidenceThreshold.TabIndex = 5;
+            this.nudConfidenceThreshold.Margin = new Padding(3, 3, 15, 3);
+            // 
+            // btnLayout
+            // 
+            this.btnLayout.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            this.btnLayout.ColumnCount = 2;
+            this.btnLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 94F));
+            this.btnLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            this.btnLayout.Controls.Add(this.btnCancel, 1, 0);
+            this.btnLayout.Controls.Add(this.btnSave, 0, 0);
+            this.btnLayout.Location = new Point(12, 278);
+            this.btnLayout.Name = "btnLayout";
+            this.btnLayout.RowCount = 1;
+            this.btnLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            this.btnLayout.Size = new Size(376, 34);
+            this.btnLayout.TabIndex = 9;
+            // 
             // btnSave
             // 
-            this.btnSave.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            this.btnSave.Location = new Point(346, 345);
+            this.btnSave.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            this.btnSave.DialogResult = DialogResult.OK;
+            this.btnSave.Location = new Point(3, 4);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new Size(89, 30);
-            this.btnSave.TabIndex = 9;
+            this.btnSave.Size = new Size(88, 26);
+            this.btnSave.TabIndex = 0;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += BtnSave_Click;
             // 
             // btnCancel
             // 
-            this.btnCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            this.btnCancel.Location = new Point(251, 345);
+            this.btnCancel.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            this.btnCancel.DialogResult = DialogResult.Cancel;
+            this.btnCancel.Location = new Point(97, 4);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new Size(89, 30);
-            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Size = new Size(88, 26);
+            this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += BtnCancel_Click;
@@ -139,11 +166,10 @@ namespace ShutterFace
             // 
             this.AcceptButton = btnSave;
             this.AutoScaleDimensions = new SizeF(9F, 20F);
-            this.AutoScaleMode = AutoScaleMode.Font;
+            this.AutoScaleMode = AutoScaleMode.Dpi;
             this.CancelButton = btnCancel;
-            this.ClientSize = new Size(444, 390);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnSave);
+            this.ClientSize = new Size(436, 316);
+            this.Controls.Add(this.btnLayout);
             this.Controls.Add(this.pnlSettings);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
@@ -155,10 +181,6 @@ namespace ShutterFace
             this.Load += SettingsForm_Load;
             this.pnlSettings.Controls.Add(this.gbConfidence);
             this.pnlSettings.Controls.Add(this.gbBlur);
-            this.gbBlur.Controls.Add(this.lblGridCellSize);
-            this.gbBlur.Controls.Add(this.nudGridCellSize);
-            this.gbConfidence.Controls.Add(this.lblConfidenceThreshold);
-            this.gbConfidence.Controls.Add(this.nudConfidenceThreshold);
             this.ResumeLayout(false);
         }
 
